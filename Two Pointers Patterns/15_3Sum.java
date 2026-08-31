4822c2629d7c8b728e51e3d3182d924ac2f5101f
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-08-31 23:32:06 +0530

    Added 3Sum Program

---
+++ Two Pointers Patterns/15_3Sum.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Solution {
+    void twoSumHelper(int f, int[] nums, List<List<Integer>> res) {
+        int i = f + 1;
+        int j = nums.length - 1;
+        while (i < j) {
+            int sum = nums[f] + nums[i] + nums[j];
+            if (sum > 0) {
+                j--;
+            } else if (sum < 0) {
+                i++;
+            } else {
+         
+                res.add(Arrays.asList(nums[f], nums[i], nums[j]));
+                i++;
+                j--;
+
+             
+                while (i < j && nums[i] == nums[i - 1]) {
+                    i++;
+                }
+       
+                while (i < j && nums[j] == nums[j + 1]) {
+                    j--;
+                }
+            }
+        }
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int f = 0; f < nums.length; f++) {
+
+            if (nums[f] > 0) {
+                break;
+            }
+     
+            if (f == 0 || nums[f] != nums[f - 1]) {
+                twoSumHelper(f, nums, res);
+            }
+        }
+        return res;
+    }
+}
